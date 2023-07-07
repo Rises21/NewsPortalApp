840126ff3117b4ada58243f6b3a2d490ead542bf
Rises21 <bangkitsetio21@gmail.com>
2023-07-07 21:23:35 +0700

    create categories collection

--- newsPortalBackend/routes/categoryRoute.js
+++ newsPortalBackend/routes/categoryRoute.js
@@ -9,8 +9,8 @@ const {
 } = require("../controllers/categoryController");
 
 router.route("/addCategory").post(protect, addCategory);
-router.route("/deleteCategory").post(protect, deleteCategory);
+router.route("/deleteCategory/:catId").post(protect, deleteCategory);
 router.route("/getAllCategories").get(getAllCategories);
-router.route("/updateCategory").put(protect, updateCategory);
+router.route("/updateCategory/:catId").put(protect, updateCategory);
 
 module.exports = router;
