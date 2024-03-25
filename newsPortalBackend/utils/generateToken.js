73a495375cf4b012980f28f8dce44d82a415bcbd
Rises21 <bangkitsetio21@gmail.com>
2024-03-25 19:48:43 +0700

    newsApp

--- newsPortalBackend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (payload) => {
-  return jwt.sign({ payload }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "30d",
-  });
-};
-
-module.exports = generateToken;
